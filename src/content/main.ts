1a29cd6c0d267331414679b31284f8db04b8fa83
Connor Skudlarek <connor.skudlarek@gmail.com>
2025-11-16 16:51:26 -0800

    feat: Update version to 0.0.2 and implement input tracking functionality

--- src/content/main.ts
+++ src/content/main.ts
@@ -1,10 +1,10 @@
 // Content Script (main.ts) - Runs on the webpage.
-
+import { identifyAndTrackInputs } from "../utils/identify_and_track_inputs";
 // Execute immediately when the script is injected
 function initialize() {
   // Log the current URL directly from the Content Script (which has access to window)
   console.log(`[CONTENT SCRIPT] Script loaded on: ${window.location.href}`);
-
+  identifyAndTrackInputs();
   // Send a message to the Service Worker to request the tab count
   // The Service Worker will be woken up by this call.
   chrome.runtime.sendMessage(
